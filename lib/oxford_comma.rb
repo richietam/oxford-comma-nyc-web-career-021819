3316bfc699f6d3d7ba1314b767bc108595bc83c5
richardtam.ny <richardtam.ny@gmail.com>
2019-02-11 04:19:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,6 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   else
     arr1 = array.pop
-    counter = 0
-    while counter < array.length - 1
     return arr1.join(",")
 
   end
