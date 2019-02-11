cf6dfaeebdc23df96a20acaf761918160979d3de
richardtam.ny <richardtam.ny@gmail.com>
2019-02-11 04:23:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    arr1 = array.pop
+    return "#{array.join(", ")}, and #{arr1}"
+  end
 end
